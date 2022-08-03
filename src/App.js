71b328dedb366f7d562687b975312c24e8ea19a9
tonylo22 <devtonyhk@gmail.com>
2022-08-03 18:00:36 +0800

    add +-x/= click handlers

--- src/App.js
+++ src/App.js
@@ -8,21 +8,23 @@ class App extends Component {
     super(props);
     this.state = {
       currentValue: "0",
-      totalValue: "0",
+      totalValue: null,
       operator: null,
       calculationChain: "0",
       arithmetics: {
-        add: (current, last) => Number(last) + Number(current),
-        minus: (current, last) => Number(last) - Number(current),
-        multiply: (current, last) => Number(last) * Number(current),
-        divide: (current, last) => Number(current) === 0? 0 : Number(last) / Number(current)
+        "+": (current, last) => Number(last) + Number(current),
+        "-": (current, last) => Number(last) - Number(current),
+        "x": (current, last) => Number(last) * Number(current),
+        "/": (current, last) => Number(current) === 0? 0 : Number(last) / Number(current)
       }
     };
     this.numClick = this.numClick.bind(this);
     this.decimalClick = this.decimalClick.bind(this);
     this.clearClick = this.clearClick.bind(this);
     this.percentageClick = this.percentageClick.bind(this);
-    this.invertClick = this.invertClick.bind(this)
+    this.invertClick = this.invertClick.bind(this);
+    this.arithmeticClick = this.arithmeticClick.bind(this);
+    this.equalClick = this.equalClick.bind(this);
   }
 
   numClick(event) {
@@ -41,10 +43,12 @@ class App extends Component {
       currentValue: updateValue,
       calculationChain: newCalChain
     });
+    console.log(this.state.totalValue);
   }
 
   decimalClick(event) {
     if (this.state.currentValue.includes('.')) {return;}
+    if (this.state.operator === null && this.state.totalValue) {return;}
     this.setState({
       currentValue: this.state.currentValue + ".",
       calculationChain: this.state.calculationChain + "."
@@ -54,27 +58,63 @@ class App extends Component {
   clearClick(event) {
     this.setState({
       currentValue: "0",
-      totalValue: "0",
+      totalValue: null,
       operator: null,
       calculationChain: "0"
     });
   }
 
   arithmeticClick(event) {
-    
+    if ("+-x/".includes(this.state.calculationChain.at(-1))) {return;}
+    const newOperator = event.target.innerHTML;
+    const newCalChain = this.state.calculationChain + newOperator;
+
+    if (this.state.operator === null) {
+      this.setState({
+        totalValue: this.state.currentValue,
+        currentValue: "",
+        operator: newOperator,
+        calculationChain: newCalChain
+      });
+    }
+    else {
+      const calculate = this.state.arithmetics[this.state.operator];
+      this.setState({
+        totalValue: calculate(this.state.currentValue, this.state.totalValue).toString(),
+        currentValue: "",
+        operator: newOperator,
+        calculationChain: newCalChain
+      });
+    }
   }
 
   equalClick(event) {
+    if (!this.state.operator || "+-x/".includes(this.state.calculationChain.at(-1))) {return;}
+    const calculate = this.state.arithmetics[this.state.operator];
+    const updateValue = calculate(this.state.currentValue, this.state.totalValue).toString();
+    this.setState({
+      totalValue: updateValue,
+      currentValue: updateValue,
+      operator: null
+    });
 
   }
 
+  // NOT finished
   percentageClick(event) {
+    if ("+-x/".includes(this.state.calculationChain.at(-1))) {return;}
+    const newCalChain = this.state.calculationChain + "x1%";
+    const calculate = this.state.arithmetics[this.state.operator];
+    const updateValue = calculate(this.state.currentValue, this.state.totalValue);
     this.setState({
-      currentValue: parseFloat((Number(this.state.currentValue) /100).toFixed(10)).toString(),
-      calculationChain: parseFloat((Number(this.state.calculationChain) /100).toFixed(10)).toString()
+      totalValue: parseFloat((updateValue /100).toFixed(10)).toString(),
+      calculationChain: newCalChain
+      // currentValue: parseFloat((Number(this.state.currentValue) /100).toFixed(10)).toString(),
+      // calculationChain: parseFloat((Number(this.state.calculationChain) /100).toFixed(10)).toString()
     });
   }
 
+  //NOT finfished
   invertClick(event) {
     this.setState({
       currentValue: (Number(this.state.currentValue) * -1).toString(),
@@ -83,10 +123,16 @@ class App extends Component {
   }
 
   render() {
+    console.log({
+      total: this.state.totalValue,
+      operator: this.state.operator,
+      current: this.state.currentValue
+    });
+
     return (
       <div id="wrapper">
         <Screen 
-          currentValue={this.state.currentValue} 
+          totalValue={this.state.totalValue} 
           calculationChain={this.state.calculationChain} 
         />
         <Keyboard clickHandlers={{
